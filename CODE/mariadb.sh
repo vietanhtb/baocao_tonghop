79af2497d8a7efc2bcc2493f3d0407dd51ae711d
unknown <pcvietanh.99@gmail.com>
2022-01-27 08:24:32 +0700

    code

--- CODE/mariadb.sh
+++ CODE/mariadb.sh
@@ -1,11 +1,23 @@
+#! /bin/bash
+
+dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+
+echo "[mariadb]" >> /etc/yum.repos.d/MariaDB.repo
+echo "name = MariaDB" >> /etc/yum.repos.d/MariaDB.repo
+echo "baseurl = https://mirrors.bkns.vn/mariadb/yum/10.3/rhel8-amd64" >> /etc/yum.repos.d/MariaDB.repo
+echo "module_hotfixes=1" >> /etc/yum.repos.d/MariaDB.repo
+echo "gpgkey=https://mirrors.bkns.vn/mariadb/yum/RPM-GPG-KEY-MariaDB" >> /etc/yum.repos.d/MariaDB.repo
+echo "gpgcheck=1" >> /etc/yum.repos.d/MariaDB.repo
+
 echo "Cài đặt mariadb"
 
-curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | bash -s -- --mariadb-server-version="mariadb-10.3"
-yum install MariaDB-server MariaDB-client -y
-systemctl enable mariadb
+dnf install -y MariaDB-server
+systemctl enable --now mariadb
 systemctl start mariadb
 mysql_secure_installation
 
-echo "Hoàn thành cài đặt mariadb"
+
 
 exit 0
